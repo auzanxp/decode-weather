a258ce3f50088becf07f801209323247a3c0fd44
Auzan <istikhoriauzan@gmail.com>
2023-05-22 20:17:14 +0700

    feat: style hourly forcast

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Forecast from "./components/Forecast";
 import ListCity from "./components/ListCity";
 import TemperatureDetails from "./components/TemperatureDetails";
 import TimeLoc from "./components/TimeLoc";
@@ -8,7 +9,9 @@ export default function App() {
       <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-500 to-blue-700 h-fit shadow-xl shadow-gray-400">
         <ListCity />
         <TimeLoc />
-        <TemperatureDetails/>
+        <TemperatureDetails />
+        <Forecast title={"Hourly Forcast"} />
+        <Forecast title={"Hourly Forcast"} />
       </div>
     </>
   );
