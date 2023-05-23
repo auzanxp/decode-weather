71acac4b4f1444f873e8ab54a92fda2abbb8b92d
Auzan <istikhoriauzan@gmail.com>
2023-05-23 22:53:13 +0700

    fix: fetch api key

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,8 @@
-// https://api.openweathermap.org/data/2.5/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid={API key}
+// https://api.openweathermap.org/data/2.5/onecall/timemachine?lat={lat}&lon={lon}&dt={time}&appid={API key}
+// https://api.openweathermap.org/data/2.5/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid={apikey}&units=metric
 
 import axios from "axios";
+import { DateTime } from "luxon";
 
 // const getWeatherData = (infoType, searchParams) => {
 //     const url = new URL(`${process.env.REACT_APP_BASE_URL}/${infoType}`);
@@ -21,7 +23,8 @@ const getWeatherData = async (infoType, searchParams) => {
         const data = await axios.get(url, { params });
         return data.data;
     } catch (error) {
-        throw new Error(error.data.message);
+        console.log(error.message)
+        throw new Error(error.message);
     }
 };
 
@@ -39,9 +42,47 @@ const mapListCurrentData = (data) => {
     return { lat, lon, temp, feels_like, temp_min, temp_max, humidity, name, country, sunrise, sunset, dt, speed, deg, gust, description, icon }
 }
 
+const mapListForecastData = (data) => {
+    let { timezone, hourly } = data
+    hourly = hourly.slice(1, 6).map(data => {
+        return {
+            title: formatToLocalTime(data.dt, timezone, 'hh:mm a'),
+            main: data.weather[0].main,
+            desc: data.weather[0].description,
+            temp: data.temp,
+            icon: data.weather[0].icon,
+            windSpeed: data.wind_speed,
+            windDeg: data.wind_deg,
+            windGust: data.wind_gust,
+        }
+    })
+
+    return { timezone, hourly }
+}
+
+export const formatToLocalTime = (
+    secs, zone, format = "cccc, dd LLL yyyy' | local time: 'hh:mm a"
+) => DateTime.fromSeconds(secs).setZone(zone).toFormat(format)
+
+export const handleIconWeather = (data) => `http://openweathermap.org/img/wn/${data}@2x.png`
+
 export const getFormatedWeatherData = async (searchParams) => {
     const formattedCurrentWeather = await getWeatherData('weather', searchParams)
         .then(mapListCurrentData)
 
-    return formattedCurrentWeather
+    const { lat, lon, dt } = formattedCurrentWeather
+    // const formattedForecastWeather = await getWeatherData(`${onecal}/${timemach}`,
+    //     {
+    //         lat,
+    //         lon,
+    //         exclude: 'current,minutely,alerts',
+    //         units: searchParams.unit
+    //     }).then(mapListForecastData)
+    const formattedForecastWeather = await getWeatherData(`onecall/timemachine`,
+        {
+            lat, lon, dt, units: 'matric',
+        }).then(mapListForecastData)
+
+
+    return { ...formattedCurrentWeather, ...formattedForecastWeather }
 }
